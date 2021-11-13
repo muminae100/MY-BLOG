98ef98fcc6202bc6655b608d499f1e881ef0c961
muminae100 <smuminaetx100@gmail.com>
2021-11-13 12:37:21 +0300

    add flask-migrate extension

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.secret_key = '92660847ee989c815a32b5ecbad887f7'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
