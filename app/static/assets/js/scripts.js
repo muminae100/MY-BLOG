164000619ed9d33fde764d835024d761c0ee96d9
muminae100 <smuminaetx100@gmail.com>
2021-11-19 01:47:47 +0300

    add delete post route

--- app/static/assets/js/scripts.js
+++ app/static/assets/js/scripts.js
@@ -12,9 +12,28 @@ function openNav() {
     $(this).parent().toggleClass('open');
   });
 
-  // category_select = document.getElementById('category');
+// Get the modal
+var modal = document.getElementById("deletePostModal");
 
-  // category_select.onchange = function() {
-  //   category = category_select.value;
-  //   alert(category)
-  // }
+// Get the button that opens the modal
+var btn = document.getElementById("deletePostBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("deletePost-close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
